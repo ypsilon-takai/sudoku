392b050dcc73e4ac308e3543356b520728caaab4
ypsilon.takai <ypsilon.takai@gmail.com>
2013-04-18 21:04:25 +0900

    Decide to change basic data structure to implement more efficient strategy.

---
+++ src/sudoku/solve_01.clj
@@ -0,0 +1,96 @@
+(ns sudoku.solve
+  (:use [clojure.pprint])
+  (:require [clojure.set :as set]))
+
+(def test-board
+  [[0 0 3 0 2 0 6 0 0]
+   [9 0 0 3 0 5 0 0 1]
+   [0 0 1 8 0 6 4 0 0]
+   [0 0 8 1 0 2 9 0 0]
+   [7 0 0 0 0 0 0 0 8]
+   [0 0 6 7 0 8 2 0 0]
+   [0 0 2 6 0 9 5 0 0]
+   [8 0 0 2 0 3 0 0 9]
+   [0 0 5 0 1 0 3 0 0]])
+
+;;
+(defn boardify [raw-data]
+  (vec (map vec (partition 9 raw-data))))
+
+
+;;
+(defn solved? [board]
+  (not-any? false?
+            (map #(and (not-any? coll? %)
+                       (not-any? zero? %))
+                 board)))
+
+;; utils
+(defn get-num [[x y] board]
+   (nth (nth board y) x))
+
+(defn low-dat
+  ([pos board] (low-dat pos board true))
+  ([[x y] board include-xy?]
+     (for [ix (range 9) :when (if include-xy?
+                                true
+                                (not= ix x))]
+       (get-in board [y ix]))))
+ 
+(defn col-dat
+  ([pos board] (col-dat pos board true))
+  ([[x y] board include-xy?]
+     (for [iy (range 9) :when (if include-xy?
+                                true
+                                (not= iy y))]
+       (get-in board [iy x]))))
+ 
+(defn box-dat
+  ([pos board] (box-dat pos board true))
+  ([[x y] board include-xy?]
+     (let [x-list (map #(+ (* 3 (quot x 3)) %) (range 3))
+           y-list (map #(+ (* 3 (quot y 3)) %) (range 3))]
+       (for [iy y-list ix x-list :when (if include-xy?
+                                         true
+                                         (not (and (= ix x)
+                                                   (= iy y))))]
+         (get-in board [iy ix])))))
+ 
+
+
+;; rule 1
+;;  if there is only one possible number, place it there.
+(defn candidates [pos board]
+  (->> ((juxt low-dat col-dat box-dat) pos board true)
+       (flatten ,,)
+       (remove #(or (coll? %) (zero? %)) ,,)
+       (set ,,)
+       (set/difference (set (range 1 10)) ,,)))
+
+(defn step-rule-1 [board]
+  (boardify
+   (for [y (range 9) x (range 9)]
+     (let [num (get-num [x y] board)]
+       (if (or (set? num)
+               (zero? num))
+         (let [can (candidates [x y] board)]
+           (if (= (count can) 1)
+             (first can)
+             can))
+         num)))))
+ 
+(defn run-rule-1 [board]
+  (let [next (step-rule-1 board)]
+    (cond (solved? next) {:solved true :board next}
+          (= board next) {:solved false :board next}
+          :t (do (pprint next)
+                 (recur next)))))
+
+
+ 
+;; rule-2 
+(defn step-rule-2 [board]
+  (for [y (range 9) x (range 9)]
+    (let [dat (get-num [x y] board)]
+      (if (set? num)
+        (let )))))
