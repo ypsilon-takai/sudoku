dc5f2c35ecfb1a674e3b6d80dbd0cfe6fc3477fd
ypsilon.takai <ypsilon.takai@gmail.com>
2013-05-27 20:13:01 +0900

    add rule applyer

--- src/sudoku/solve.clj
+++ src/sudoku/solve.clj
@@ -1,9 +1,15 @@
 (ns sudoku.solve
-  (:use [sudoku.interface]
-        [sudoku.util]
-        [sudoku.rules])
   (:require [clojure.set :as set]
-            [clojure.math.combinatorics :as combo]))
+            [clojure.math.combinatorics :as combo]
+            [sudoku.interface :as if]
+            [sudoku.util :as util]
+            [sudoku.rules :as rules]))
 
 ;;
+(defn apply-rules [board]
+  (let [new-board (reduce (fn [bd rule] (rule bd)) board rules/rule-list)]
+    (if (= board new-board)
+      board
+      (recur new-board))))
+
 
