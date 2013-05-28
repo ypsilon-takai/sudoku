cf3a91cc1545206e78718f131b65191ede2d4153
ypsilon.takai <ypsilon.takai@gmail.com>
2013-05-28 19:52:38 +0900

    adding project euler problem solving funcs: 01

--- src/sudoku/solve.clj
+++ src/sudoku/solve.clj
@@ -1,7 +1,7 @@
 (ns sudoku.solve
   (:require [clojure.set :as set]
             [clojure.math.combinatorics :as combo]
-            [sudoku.interface :as if]
+            [sudoku.interface :as io]
             [sudoku.util :as util]
             [sudoku.rules :as rules]))
 
@@ -12,4 +12,21 @@
       board
       (recur new-board))))
 
+
+(defn solve-pe96 []
+  (->> "d:/userdata/q3197c/workspace/clojure/sudoku/src/sudoku/sudoku.txt"
+       (io/read-euler-data ,,)
+       (map (fn [[title dat]] [title (apply-rules (util/update-candidate dat))]) ,,)))
+
+(defn pe96 []
+  (with-open [outfile (clojure.java.io/writer "d:/userdata/q3197c/Desktop/pe86_out.txt")]
+    (binding [*out* outfile]
+      (dorun (for [[title dat] (solve-pe96)]
+               (dorun (println title)
+                      (io/print-board (apply-rules (util/update-candidate dat)))))))))
+
+
+(defn solve-pe96 []
+  (let [problem-list (io/read-euler-data "d:/userdata/q3197c/workspace/clojure/sudoku/src/sudoku/sudoku.txt")]
+    ))
 
