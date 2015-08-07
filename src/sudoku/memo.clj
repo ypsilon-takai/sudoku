062551ef24333c8958468f4d9a9d928fadb74228
takaiy <ypsilon.takai@gmail.com>
2015-08-07 19:40:21 +0900

    support files

--- src/sudoku/memo.clj
+++ src/sudoku/memo.clj
@@ -11,3 +11,28 @@
 
 (defn boxdat [b p]
   (zipmap (box-pos-list p) (map #(get-val b %) (box-pos-list p))))
+
+(def td (second (first (sudoku.interface/read-euler-data "file://d:/msys64/home/q3197c/workspace/yosi/sudoku/src/sudoku/sudoku.txt"))))
+
+(def d (sudoku.util/update-candidate (sudoku.util/create-board td)))
+
+
+(defn solve-pe96 []
+  (->> "file://d:/msys64/home/q3197c/workspace/yosi/sudoku/src/sudoku/sudoku.txt"
+       (io/read-euler-data ,,)
+       (map (fn [[title dat]] [title (rules/apply-rules (util/update-candidate (util/create-board dat)))]) ,,)))
+
+(defn solve-pe96 []
+  (->> "file://d:/msys64/home/q3197c/workspace/yosi/sudoku/src/sudoku/p096_sudoku.txt"
+       (io/read-euler-data ,,)
+       (map (fn [[title dat]] [title (rules/apply-rules (util/update-candidate (util/create-board dat)))]) ,,)))
+
+(map first (filter #(false? (util/solved? (second %))) ans1))
+;;=> ("Grid 06" "Grid 07" "Grid 42" "Grid 48" "Grid 49" "Grid 50")
+
+(first (nth ans1 5))
+
+(count (:history (second (nth ans1 5))))
+181
+
+(map first (partition-by identity (map :rule (:history (second (nth ans1 5))))))
